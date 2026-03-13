f2629f04b495c30a27823f84bc837c3197fd2f40
djsydney04 <dylanmitic@gmail.com>
2026-03-12 17:25:25 -0700

    monostack.h added and complete

--- pa-3/monoStack.h
+++ pa-3/monoStack.h
@@ -6,5 +6,70 @@
 template <typename T>
 class MonoStack {
     public:
-        MonoStack();
-}
+        MonoStack(int capacity, char order) {
+            if (capacity <= 0) {
+                throw std::invalid_argument("Size must be greater than 0");
+            }
+            if (order != 'a' && order != 'd') {
+                throw std::invalid_argument("Order must be 'a' or 'd'");
+            }
+            this->capacity_ = capacity;
+            this->stack = new T[capacity];
+            this->top = -1;
+            this->order = order;
+        }
+
+        ~MonoStack() {
+            delete[] stack;
+        }
+
+        bool isEmpty() const {
+            return top == -1;
+        }
+
+        bool isFull() const {
+            return top == capacity_ - 1;
+        }
+
+        size_t size() const {
+            return top + 1;
+        }
+
+        size_t capacity() const {
+            return capacity_;
+        }
+
+        T peek() const {
+            if (isEmpty()) {
+                throw std::underflow_error("Stack is empty");
+            }
+            return stack[top];
+        }
+
+        T pop() {
+            if (isEmpty()) {
+                throw std::underflow_error("Stack is empty");
+            }
+            return stack[top--];
+        }
+
+        void push(T value) {
+            if (order == 'a') {
+                while (!isEmpty() && peek() < value) {
+                    pop();
+                }
+            } else if (order == 'd') {
+                while (!isEmpty() && peek() > value) {
+                    pop();
+                }
+            }
+            stack[++top] = value;
+        }
+    private:
+        T* stack;
+        int top;
+        int capacity_;
+        char order;
+};
+
+#endif
